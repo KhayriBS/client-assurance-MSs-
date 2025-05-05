e2d8128c7a4b86e99c66a37cc86db5b6ed61fc80
Khayri BEN SALAH <khiareddine.bensalah@esprit.tn>
2025-05-05 21:12:03 +0100

    add @ComponentScan to main class

--- Assurance-service/src/main/java/com/microservices/assuranceservice/AssuranceServiceApplication.java
+++ Assurance-service/src/main/java/com/microservices/assuranceservice/AssuranceServiceApplication.java
@@ -2,8 +2,16 @@ package com.microservices.assuranceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.microservices.assuranceservice",
+        "com.microservices.controller",
+        "com.microservices.config",
+        "com.microservices.dto",
+        "com.microservices.exceptions",
+})
 public class AssuranceServiceApplication {
 
     public static void main(String[] args) {
--- Client-service/src/main/java/com/microservices/clientservice/ClientServiceApplication.java
+++ Client-service/src/main/java/com/microservices/clientservice/ClientServiceApplication.java
@@ -3,9 +3,17 @@ package com.microservices.clientservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableFeignClients
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.microservices.clientservice",
+        "com.microservices.controller",
+        "com.microservices.config",
+        "com.microservices.dto",
+        "com.microservices.exceptions",
+})
 public class ClientServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClientServiceApplication.class, args);
